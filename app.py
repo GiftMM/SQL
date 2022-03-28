b509fe7d62c767a792999a41e0010c5727aafebc
GiftMM <giftmmwanangombe@gmail.com>
2022-03-28 14:18:36 +0200

    Assignment

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+import database
+
+app = Flask(__name__)
+
+@app.route('/')
+def suppliers():
+    suppliers = database.get_all_suppliers()
+    return render_template('index.html', suppliers=suppliers)
+
+@app.route('/suppliers/<int:supplier_id>')
+def products(supplier_id):
+    products = database.get_supplier_products(supplier_id)
+    suppliers = database.query("""SELECT CompanyName FROM Supplier WHERE Id = ?""", supplier_id)
+    supplier = suppliers[0]
+    suppliername=supplier['CompanyName']
+    return render_template('products.html', products=products, supplier=suppliername)
+
+
+@app.route('/categories')
+def categories():
+    categories = database.get_categories()
+    return render_template('category.html', categories=categories)
+
+
+@app.route('/categories/<int:category_id>')
+def productcategory(category_id):
+    products = database.get_category_products(category_id)
+    categories = database.query("""SELECT CategoryName FROM Category WHERE Id = ?""", category_id)
+    category = categories[0]
+    categoryname = category['CategoryName']
+    return render_template('productcategory.html', products=products, category=categoryname)
---
+++ database.py
@@ -0,0 +1,42 @@
+import sqlite3
+
+
+def query(query_text, *param):
+    conn = sqlite3.connect ('Northwind_large.sqlite')
+    cur = conn.cursor()
+    cur.execute(query_text, param)
+
+    column_names = []
+    for column in cur.description:
+        column_names.append(column[0])
+
+    print(column_names)
+    rows = cur.fetchall()
+
+    dicts= []
+    for row in rows:
+        d = dict(zip(column_names, row))
+        dicts.append(d)
+    conn.close()
+    return dicts
+
+
+def get_all_suppliers():
+    return query("""SELECT COUNT (Product.Id) AS ProductCount, Supplier.CompanyName, Supplier.City, Supplier.Country, Supplier.Id
+FROM Product
+INNER JOIN SUpplier
+	ON Product.SupplierId = Supplier.Id
+	
+GROUP BY CompanyName""")
+
+def get_supplier_products(supplier_id):
+    return query("""SELECT * FROM Product WHERE SupplierId = ? """, supplier_id )
+
+def get_categories():
+    return query("""SELECT Category.CategoryName, Category.Description, Category.Id, COUNT (Product.CategoryId) AS ProductCount FROM Product
+INNER JOIN Category
+	ON Product.CategoryId = Category.Id
+GROUP BY CategoryId """)
+
+def get_category_products(category_id):
+    return query("""SELECT * FROM Product INNER JOIN Supplier ON Product.SupplierId = Supplier.Id WHERE CategoryId = ? """, category_id )
